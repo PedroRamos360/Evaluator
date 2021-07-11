f0d54c15567b650489035826d3c14be15ae327cb
Pedro Ramos <53490820+PedroRamos360@users.noreply.github.com>
2021-07-11 12:12:22 -0300

    Estilização do conteúdo da mensagem

---
+++ evaluator-frontend/src/components/MessageContent.js
@@ -0,0 +1,29 @@
+import React from 'react';
+
+import styles from '../styles/components/MessageContent.module.css';
+import { FiX } from "react-icons/fi";
+
+export default function MessageContent() {
+	return (
+		<main className={styles.background}>
+			<header className={styles.header}>
+				<div>
+					<h1>Volume de atividades</h1>
+					<h2 className={styles.sender}>Anônimo</h2>
+				</div>
+				<button className={styles.closeButton}>
+					<FiX />
+				</button>
+			</header>
+			<p className={styles.messageText}>
+				Bom dia professor, estou escrevendo esta mensagem para reclamar sobre o
+				volume de atividades que estão sendo passadas ultimamente. Além disso me
+				parece que muitas delas não foram muito bem planejadas e não contribuem
+				para o nosso aprendizado na matéria em questão.
+			</p>
+			<footer className={styles.footer}>
+				<button className={styles.markAsReadButton}>Marcar como lida</button>
+			</footer>
+		</main>
+	)
+}
--- evaluator-frontend/src/pages/TeacherPage.js
+++ evaluator-frontend/src/pages/TeacherPage.js
@@ -4,12 +4,18 @@ import styles from '../styles/pages/TeacherPage.module.css';
 import TopLeftLines from '../components/TopLeftLines';
 import BottomRightLines from '../components/BottomRightLines';
 import Message from '../components/Message';
+import MessageContent from '../components/MessageContent';
 
 export default function TeacherPage() {
+	function messageClicked() {
 
+	}
 	return (
 		<>
 			<TopLeftLines />
+			<div className={`${styles.teacherMessageContent} `}>
+				<MessageContent />
+			</div>
 			<main className={styles.teacherContainer}>
 				<h1 className={styles.teacherTitle}>Mensagens</h1>
 				<div className={styles.teacherMessages}>
