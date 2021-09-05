d1f319b19b348dffc6dca9767398fb3162e697d2
Pedro Ramos <53490820+PedroRamos360@users.noreply.github.com>
2021-09-05 13:37:51 -0300

    frontend completo

--- evaluator-frontend/src/pages/TeacherPage.js
+++ evaluator-frontend/src/pages/TeacherPage.js
@@ -1,4 +1,6 @@
 import React, { useState } from 'react';
+import { useHistory } from 'react-router-dom';
+
 
 import styles from '../styles/pages/TeacherPage.module.css';
 import TopLeftLines from '../components/TopLeftLines';
@@ -7,6 +9,7 @@ import Message from '../components/Message';
 import MessageContent from '../components/MessageContent';
 
 export default function TeacherPage() {
+	const history = useHistory();
 	const [isHidden, setIsHidden] = useState(true);
 	function messageClicked() {
 		setIsHidden(false);
@@ -16,13 +19,23 @@ export default function TeacherPage() {
 			<TopLeftLines />
 			<MessageContent isHidden={isHidden} setIsHidden={setIsHidden}/>
 			<main className={styles.teacherContainer}>
-				<h1 className={styles.teacherTitle}>Mensagens</h1>
+				<header className={styles.header}>
+					<div className={`${styles.logOutContainer} ${styles.logOutFake}`}></div>
+					<h1 className={styles.teacherTitle}>Mensagens</h1>
+					<div className={styles.logOutContainer}>
+						<h3 className={styles.usernameText}>
+							PedroRamos360
+						</h3>
+						<button onClick={() => { history.push('/')}} className={styles.button}>Desconectar</button>
+					</div>
+				</header>
 				<div className={styles.teacherMessages}>
 					{ }
 					<Message onClick={messageClicked} />
 					<Message onClick={messageClicked} />
 					<Message onClick={messageClicked} />
 					<Message onClick={messageClicked} />
+
 				</div>
 			</main>
 			<BottomRightLines />
