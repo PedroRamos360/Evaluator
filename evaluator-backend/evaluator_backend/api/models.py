969cd45896328b1f7502703a3cb4809fa5ae6d9e
Pedro Ramos <53490820+PedroRamos360@users.noreply.github.com>
2021-09-20 15:44:06 -0300

    criação dos models e arquivo de passo a passo de criação de projeto django

--- evaluator-backend/evaluator_backend/api/models.py
+++ evaluator-backend/evaluator_backend/api/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+class Message(models.Model):
+    sender = models.CharField(max_length=32, unique=False)
+    subject = models.CharField(max_length=64, unique=False)
+
+
+class User(models.Model):
+    username = models.CharField(max_length=32, unique=True)
+    password = models.CharField(max_length=256, unique=False)
+    messages = models.ManyToManyField(Message, verbose_name="list of messages")
+    
+
+
+
--- evaluator-backend/evaluator_backend/api/views.py
+++ evaluator-backend/evaluator_backend/api/views.py
@@ -1,7 +1,23 @@
-from django.shortcuts import render
 from django.http import HttpResponse
+from rest_framework import generics
 
 # Create your views here.
 
 def test(request):
     return HttpResponse("teste")
+
+
+class GetUser(generics.ListAPIView):
+    pass
+
+
+class CreateUser():
+    pass
+
+
+class GetMessages():
+    pass
+
+
+class SendMessage():
+    pass
