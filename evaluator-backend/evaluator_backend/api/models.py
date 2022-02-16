a92a6b7cd7ab68f23f5d60c8259d1ae1be46eff3
Pedro Ramos <53490820+PedroRamos360@users.noreply.github.com>
2022-02-16 10:09:01 -0300

    tamanhos ajustados

--- evaluator-backend/evaluator_backend/api/models.py
+++ evaluator-backend/evaluator_backend/api/models.py
@@ -1,12 +1,12 @@
 from django.db import models
 
 class Message(models.Model):
-    sender = models.CharField(max_length=32, unique=False)
-    receiver = models.CharField(max_length=32, unique=False)
-    subject = models.CharField(max_length=64, unique=False)
+    sender = models.CharField(max_length=50, unique=False)
+    receiver = models.CharField(max_length=50, unique=False)
+    subject = models.CharField(max_length=100, unique=False)
     content = models.CharField(max_length=500, unique=False)
 
 
 class User(models.Model):
-    username = models.CharField(max_length=32, unique=True)
+    username = models.CharField(max_length=50, unique=True)
     password = models.CharField(max_length=256, unique=False)
